23c17c033a00c6ca11bef8d92d63d5632db5fa58
twopyramids <sunburnmanager@gmail.com>
2025-10-31 03:26:01 +0000

    fix: remove PlaygroundWindow.svelte

---
+++ app/src/lib/rnd/windows/PlaygroundWindow.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import WindowBase from './WindowBase.svelte';
+
+	const { windowID, owner }: { windowID: string; owner: string } = $props();
+</script>
+
+<WindowBase>
+	{windowID}
+	{owner}
+</WindowBase>
