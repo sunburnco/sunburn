68c19494d77e53cdb5593358df7afdc5e9a0b3b3
twopyramids <twopyramids@sunburn.co>
2026-03-01 00:42:50 -0600

    feat(pwa): add title for settings and login pages

--- pwa/src/routes/+layout.svelte
+++ pwa/src/routes/+layout.svelte
@@ -10,7 +10,6 @@
 	import { Track } from 'livekit-client';
 	import { themeChange } from 'theme-change';
 
-	import { page } from '$app/state';
 	import AudioTrackPlayer from '$lib/components/AudioTrackPlayer.svelte';
 	import { applicationStart } from '$lib/sunburn/applicationStart';
 	import { call, type CallUserID_t } from '$lib/sunburn/call.svelte';
@@ -24,8 +23,6 @@
 
 	themeChange(false);
 	applicationStart();
-
-	const isChannel = $derived(Boolean(page.params.channelID) || Boolean(page.params.dmID));
 
 	const micTrackIDs = $derived.by(() => {
 		const tracks: Record<CallUserID_t, string> = {};
@@ -79,12 +76,7 @@
 <!-- TODO use env() and CSS safe areas -->
 <div class="drawer grow md:drawer-open">
 	<input id="rootDrawerInput" type="checkbox" class="drawer-toggle" />
-	<div
-		class={[
-			'drawer-content flex h-dvh flex-col items-center justify-center bg-base-300',
-			!isChannel && 'p-1',
-		]}
-	>
+	<div class="drawer-content flex h-dvh flex-col items-center justify-center bg-base-300">
 		{@render children()}
 		<div class="absolute top-0 left-0">
 			<label for="rootDrawerInput" class="drawer-button btn btn-square btn-ghost btn-sm md:hidden">
