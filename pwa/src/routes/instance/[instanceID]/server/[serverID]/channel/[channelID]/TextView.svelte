3718d88abcb59baebede032c230efd486dd86d29
twopyramids <twopyramids@sunburn.co>
2026-03-02 00:16:14 -0600

    feat: virtual list for dms

--- pwa/src/routes/instance/[instanceID]/server/[serverID]/channel/[channelID]/TextView.svelte
+++ pwa/src/routes/instance/[instanceID]/server/[serverID]/channel/[channelID]/TextView.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { LucideLoaderCircle } from '@lucide/svelte';
+	import { LucideLoaderCircle, LucidePackageOpen } from '@lucide/svelte';
 	import { DateTime } from 'luxon';
 	import { tick } from 'svelte';
 	import { VList, type VListHandle } from 'virtua/svelte';
@@ -35,7 +35,7 @@
 		let _ = instanceID;
 		_ = serverID;
 		_ = channelID;
-		scrollRef.scrollTo(scrollRef.getScrollSize());
+		scrollRef.scrollToIndex(messages.length - 1, { align: 'end' });
 	});
 
 	const sendMessage = async (content: string) => {
@@ -96,12 +96,17 @@
 					sunburn[instanceID].servers[serverID].channels[channelID].messages[i - 1 - 1]
 				: record}
 		{#if i === 0}
-			{#if !noMoreMessages}
-				<div class="flex w-full justify-center p-2 opacity-50">
+			{#if messages.length === 1}
+				<div class="flex w-full flex-col items-center justify-center p-2 opacity-50 select-none">
+					<LucidePackageOpen class="size-6" />
+					<p>Nothing to display</p>
+				</div>
+			{:else if !noMoreMessages}
+				<div class="flex w-full justify-center p-2 opacity-50 select-none">
 					<LucideLoaderCircle class="size-6 animate-spin" />
 				</div>
 			{:else}
-				<div class="flex w-full justify-center p-2 text-sm opacity-50">
+				<div class="flex w-full justify-center p-2 text-sm opacity-50 select-none">
 					You&apos;ve reached the beginning of #{sunburn[instanceID].servers[serverID].channels[
 						channelID
 					].record.name}
@@ -121,71 +126,6 @@
 		{/if}
 	{/snippet}
 </VList>
-
-<!-- <div class="flex size-full max-h-full items-stretch overflow-hidden">
-	<div
-		class="relative box-border flex grow flex-col overflow-y-auto"
-		bind:this={scrollRef}
-		onscroll={async () => {
-			if (scrollRef.scrollTop !== 0 || noMoreMessages) {
-				return;
-			}
-
-			if (sunburn[instanceID].servers[serverID].channels[channelID].messages.length === 0) {
-				noMoreMessages = true;
-				return;
-			}
-
-			const count = await fetchChannelMessagesBefore(
-				instanceID,
-				serverID,
-				channelID,
-				sunburn[instanceID].servers[serverID].channels[channelID].messages[0].created,
-				null,
-			);
-
-			if (count === 0) {
-				noMoreMessages = true;
-			} else {
-				scrollRef.scrollTop = 32;
-			}
-		}}
-		{@attach createStaydown({ pauseOnUserScroll: true })}
-	>
-		{#if !noMoreMessages}
-			<div class="flex w-full justify-center p-2 opacity-50">
-				<LucideLoaderCircle class="size-6 animate-spin" />
-			</div>
-		{:else}
-			<div class="flex w-full justify-center p-2 text-sm opacity-50">
-				You&apos;ve reached the beginning of #{sunburn[instanceID].servers[serverID].channels[
-					channelID
-				].record.name}
-			</div>
-		{/if}
-		{#each sunburn[instanceID].servers[serverID].channels[channelID].messages as record, i (record.id)}
-			{@const prevRecord =
-				i > 0 ? sunburn[instanceID].servers[serverID].channels[channelID].messages[i - 1] : record}
-			<Message
-				{instanceID}
-				{record}
-				first={i === 0}
-				cozy={i !== 0 &&
-					prevRecord.from === record.from &&
-					DateTime.fromSQL(record.created)
-						.diff(DateTime.fromSQL(prevRecord.created))
-						.as('minutes') < 5}
-			/>
-		{:else}
-			<div
-				class="select-none opacity-50 mt-2 gap-1 w-full flex items-center justify-center flex-col"
-			>
-				<LucidePackageOpen class="size-6" />
-				<div>Nothing to display</div>
-			</div>
-		{/each}
-	</div>
-</div> -->
 
 <div class="w-full">
 	<Editor onSend={sendMessage} />
